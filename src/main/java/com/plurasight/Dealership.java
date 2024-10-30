ae8704e8dc7ae7e41e0111df2c56d710e4ec23dc
AlexWilliams412 <alexw3428@gmail.com>
2024-10-30 15:35:08 -0400

    Finished CarDealership

---
+++ src/main/java/com/plurasight/Dealership.java
@@ -0,0 +1,14 @@
+package com.plurasight;
+
+import javax.imageio.plugins.bmp.BMPImageWriteParam;
+
+public class Dealership {
+    (Alex Dealership,554 Jackson road,412-892-7785)
+ Bmw m4(min 79,100,max 83,200)
+ (Germany,m4 ) Bmw
+            (min year 2017,max year 2025) bmw m4
+     getVehicelsbycolor(Black Bmw m4)
+         GetVeicelbyMileage(max 120,000 min 60,000)
+           GetVeicleByType(Bmw m4 2024)
+    addVeichle(Honda Acord)
+      RemoveVeichle(Bmw m4)
---
+++ src/main/java/com/plurasight/UserInterface.java
@@ -0,0 +1,9 @@
+package com.plurasight;
+
+public class UserInterface
+{ProcessGetByPriceRequest(Vaule based pricing)
+ ProcessGetByYourRequest()
+processGetByColorRequest(On the website)
+ processGetByMilageRequest()
+  processRemoveVehicleReuest()
+   
